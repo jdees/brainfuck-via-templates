70d145dfb6a7b05035f0fd8c5d60ec00094e42c2
Jonathan Dees <JonathanXXX@gmx.de>
2017-11-15 21:03:04 +0100

    * loop commands [ and ] work now
    Also converted tabs to 2 spaces.

--- BrainFuckCompileTime.cpp
+++ BrainFuckCompileTime.cpp
@@ -20,7 +20,7 @@ using bf_data_t = char;
 const bf_data_t data_zero = 0;
 using bf_command_t = char;
 const bf_command_t command_terminal = '\0';
-const bf_command_t command_next = '\255';
+const bf_command_t command_next = 127;
 
 
 template <bf_command_t... Command> struct Code {};
@@ -35,14 +35,14 @@ template <bool DoFail> struct FailMissingEnclosingBracket { static_assert(!DoFai
 
 template < typename CodeStack, bf_command_t C, typename TheTapeLeft, typename TheTapeRight, typename TheInput, typename TheOutput > struct Program
 {
-	using run = FailInternalError<true>();
+  using run = FailInternalError<true>();
 };
 
 // finished executing
 template < typename TheCodeStack, typename TheTapeLeft, typename TheTapeRight, typename TheInput, typename TheOutput >
 struct Program< TheCodeStack, command_terminal, TheTapeLeft, TheTapeRight, TheInput, TheOutput >
 {
-	using run = TheOutput;
+  using run = TheOutput;
 };
 
 
@@ -52,94 +52,96 @@ template < typename TheCodeStack, bool TheIsZero > struct NextCodeStack;
 
 template < typename TheCodeLeft, bool TheIsZero > struct NextCodeStack< CodeStack< TheCodeLeft, Code<> >, TheIsZero >
 {
-	static const bf_command_t command = command_terminal;
-	using stack = CodeStack< TheCodeLeft, Code<> >;
+  static const bf_command_t command = command_terminal;
+  using stack = CodeStack< TheCodeLeft, Code<> >;
 };
 
 template < bf_command_t... CL, bf_command_t CRHead, bf_command_t... CRTail, bool TheIsZero > struct NextCodeStack< CodeStack< Code< CL...>, Code< CRHead, CRTail... > >, TheIsZero  >
 {
-	static const bf_command_t command = CRHead;
-	static_assert(command != '[', "internal compile error"); //< compiler should prefer specialization
-	static_assert(command != ']', "internal compile error"); //< compiler should prefer specialization
-	using stack = CodeStack< Code< CRHead, CL... >, Code< CRTail... > >;
+  static const bf_command_t command = CRHead;
+  static_assert(command != '[', "internal compile error"); //< compiler should prefer specialization
+  static_assert(command != ']', "internal compile error"); //< compiler should prefer specialization
+  using stack = CodeStack< Code< CRHead, CL... >, Code< CRTail... > >;
 };
 
 template < bf_command_t... CL, bf_command_t... CRTail > struct NextCodeStack< CodeStack< Code< CL... >, Code< '[', CRTail... > >, true  >
 {
-	using next_code_stack = NextCodeStack< CodeStack< Code< '[', CL... >, Code< CRTail... > >, true >;
-	static const bf_command_t command = next_code_stack::command;
-	using stack = typename next_code_stack::stack;
+  using next_code_stack = NextCodeStack< CodeStack< Code< '[', CL... >, Code< CRTail... > >, true >;
+  static const bf_command_t command = next_code_stack::command;
+  using stack = typename next_code_stack::stack;
 };
 
 template < class CodeStack, int NumBrackets > struct ScanForward;
 // required? don't think so since we have ']',1 => finished
 template < class CodeStack > struct ScanForward< CodeStack, 0 >
 {
-	using result = CodeStack;
+  using result = CodeStack;
 };
 template < bf_command_t... CL, bf_command_t CRHead, bf_command_t... CRTail, int TheNumBrackets > struct ScanForward < CodeStack< Code<CL...>, Code<CRHead, CRTail...> >, TheNumBrackets >
 {
-	using result = typename ScanForward< CodeStack< Code< CRHead, CL... >, Code< CRTail... > >, TheNumBrackets >::result;
+  using result = typename ScanForward< CodeStack< Code< CRHead, CL... >, Code< CRTail... > >, TheNumBrackets >::result;
 };
 template < bf_command_t... CL, bf_command_t... CRTail, int TheNumBrackets > struct ScanForward < CodeStack< Code<CL...>, Code<'[', CRTail...> >, TheNumBrackets >
 {
-	using result = typename ScanForward< CodeStack< Code< '[', CL... >, Code< CRTail... > >, TheNumBrackets + 1 >::result;
+  using result = typename ScanForward< CodeStack< Code< '[', CL... >, Code< CRTail... > >, TheNumBrackets + 1 >::result;
 };
 template < bf_command_t... CL, bf_command_t... CRTail, int TheNumBrackets > struct ScanForward < CodeStack< Code<CL...>, Code<']', CRTail...> >, TheNumBrackets >
 {
-	using result = typename ScanForward< CodeStack< Code< ']', CL... >, Code< CRTail... > >, TheNumBrackets - 1 >::result;
+  using result = typename ScanForward< CodeStack< Code< ']', CL... >, Code< CRTail... > >, TheNumBrackets - 1 >::result;
 };
 template < bf_command_t... CL, bf_command_t... CRTail > struct ScanForward < CodeStack< Code<CL...>, Code<']', CRTail...> >, 1 >
 {
-	using result = CodeStack< Code< ']', CL... >, Code< CRTail... > >;
+  using result = CodeStack< Code< ']', CL... >, Code< CRTail... > >;
 };
 
 template < bf_command_t... CL, bf_command_t... CRTail > struct NextCodeStack< CodeStack< Code< CL... >, Code< '[', CRTail... > >, false  >
 {
-	using code_stack_scan = typename ScanForward < CodeStack< Code< '[', CL... >, Code< CRTail... > >, 1 >::result;
-	using next_code_stack = NextCodeStack< code_stack_scan, false >;
-	static const bf_command_t command = next_code_stack::command;
-	using stack = typename next_code_stack::stack;
+  using code_stack_scan = typename ScanForward < CodeStack< Code< '[', CL... >, Code< CRTail... > >, 1 >::result;
+  using next_code_stack = NextCodeStack< code_stack_scan, false >;
+  static const bf_command_t command = next_code_stack::command;
+  using stack = typename next_code_stack::stack;
 };
 
 
 template < bf_command_t... CL, bf_command_t... CRTail > struct NextCodeStack< CodeStack< Code< CL... >, Code< ']', CRTail... > >, false  >
 {
-	using next_code_stack = NextCodeStack< CodeStack< Code< ']', CL... >, Code< CRTail... > >, false >;
-	static const bf_command_t command = next_code_stack::command;
-	using stack = typename next_code_stack::stack;
+  using fail = FailInternalError<true>;
+  using next_code_stack = NextCodeStack< CodeStack< Code< ']', CL... >, Code< CRTail... > >, false >;
+  static const bf_command_t command = next_code_stack::command;
+  using stack = typename next_code_stack::stack;
 };
 
 template < class CodeStack, int NumBrackets > struct ScanBackward;
 // required? don't think so since we have ']',1 => finished
 template < class CodeStack > struct ScanBackward< CodeStack, 0 >
 {
-	using result = CodeStack;
+  using result = CodeStack;
 };
 template < bf_command_t CLHead, bf_command_t... CLTail, bf_command_t... CR, int TheNumBrackets > struct ScanBackward < CodeStack< Code<CLHead, CLTail...>, Code<CR...> >, TheNumBrackets >
 {
-	using result = typename ScanBackward< CodeStack< Code< CLTail... >, Code< CLHead, CR... > >, TheNumBrackets >::result;
+  using result = typename ScanBackward< CodeStack< Code< CLTail... >, Code< CLHead, CR... > >, TheNumBrackets >::result;
 };
 template < bf_command_t... CLTail, bf_command_t... CR, int TheNumBrackets > struct ScanBackward < CodeStack< Code<']', CLTail...>, Code<CR...> >, TheNumBrackets >
 {
-	using result = typename ScanBackward< CodeStack< Code< CLTail... >, Code< ']', CR... > >, TheNumBrackets + 1 >::result;
+  using result = typename ScanBackward< CodeStack< Code< CLTail... >, Code< ']', CR... > >, TheNumBrackets + 1 >::result;
 };
 template < bf_command_t... CLTail, bf_command_t... CR, int TheNumBrackets > struct ScanBackward < CodeStack< Code<'[', CLTail...>, Code< CR...> >, TheNumBrackets >
 {
-	using result = typename ScanBackward< CodeStack< Code< CLTail... >, Code< '[', CR... > >, TheNumBrackets - 1 >::result;
+  using result = typename ScanBackward< CodeStack< Code< CLTail... >, Code< '[', CR... > >, TheNumBrackets - 1 >::result;
 };
 template < bf_command_t... CLTail, bf_command_t... CR > struct ScanBackward < CodeStack< Code<'[', CLTail...>, Code< CR...> >, 1 >
 {
-	using result = CodeStack< Code< CLTail... >, Code< '[', CR... > >;
+  using result = CodeStack< Code< CLTail... >, Code< '[', CR... > >;
 };
 
 
 template < bf_command_t... CL, bf_command_t... CRTail > struct NextCodeStack< CodeStack< Code< CL... >, Code< ']', CRTail... > >, true  >
 {
-	using code_stack_scan_back = typename ScanBackward < CodeStack< Code<  CL... >, Code< ']', CRTail... > >, 1 >::result;
-	using next_code_stack = NextCodeStack< code_stack_scan_back, true >;
-	static const bf_command_t command = next_code_stack::command;
-	using stack = typename next_code_stack::stack;
+  using fail = FailInternalError<true>;
+  using code_stack_scan_back = typename ScanBackward < CodeStack< Code<  CL... >, Code< ']', CRTail... > >, 1 >::result;
+  using next_code_stack = NextCodeStack< code_stack_scan_back, true >;
+  static const bf_command_t command = next_code_stack::command;
+  using stack = typename next_code_stack::stack;
 };
 
 
@@ -147,8 +149,15 @@ template < bf_command_t... CL, bf_command_t... CRTail > struct NextCodeStack< Co
 template < typename TheCodeStack, typename TheTapeLeft, bf_data_t RDataHead, bf_data_t... RDataTail, typename TheInput, typename TheOutput >
 struct Program< TheCodeStack, command_next, TheTapeLeft, TapeRight<RDataHead, RDataTail...>, TheInput, TheOutput >
 {
-	using next_code_stack = NextCodeStack< TheCodeStack, RDataHead == data_zero >;
-	using run = typename Program< typename next_code_stack::stack, next_code_stack::command, TheTapeLeft, TapeRight<RDataHead, RDataTail...>, TheInput, TheOutput >::run;
+  using next_code_stack = NextCodeStack< TheCodeStack, RDataHead != data_zero >;
+  using run = typename Program< typename next_code_stack::stack, next_code_stack::command, TheTapeLeft, TapeRight<RDataHead, RDataTail...>, TheInput, TheOutput >::run;
+};
+
+template < typename TheCodeStack, typename TheTapeLeft, typename TheInput, typename TheOutput >
+struct Program< TheCodeStack, command_next, TheTapeLeft, TapeRight<>, TheInput, TheOutput >
+{
+  using next_code_stack = NextCodeStack< TheCodeStack, data_zero != data_zero >;
+  using run = typename Program< typename next_code_stack::stack, next_code_stack::command, TheTapeLeft, TapeRight<data_zero>, TheInput, TheOutput >::run;
 };
 
 
@@ -156,7 +165,7 @@ struct Program< TheCodeStack, command_next, TheTapeLeft, TapeRight<RDataHead, RD
 template < typename TheCode, bf_command_t TheC, typename TheTapeLeft, typename TheInput, typename TheOutput >
 struct Program< TheCode, TheC, TheTapeLeft, TapeRight<>, TheInput, TheOutput >
 {
-	using run = Program<TheCode,TheC,TheTapeLeft,TapeRight< data_zero >, TheInput, TheOutput >;
+  using run = Program<TheCode,TheC,TheTapeLeft,TapeRight< data_zero >, TheInput, TheOutput >;
 };
 
 // command '.'
@@ -173,7 +182,7 @@ struct Program< TheCode, ',', TheTapeLeft, TapeRight<RDataHead, RDataTail...>, I
 template < typename TheCode, typename TheTapeLeft, typename TheTapeRight, typename TheOutput >
 struct Program< TheCode, ',', TheTapeLeft, TheTapeRight, Input<>, TheOutput >
 {
-	using run = FailInput<true>;
+  using run = FailInput<true>;
 };
 
 
@@ -181,68 +190,68 @@ struct Program< TheCode, ',', TheTapeLeft, TheTapeRight, Input<>, TheOutput >
 template < typename TheCode, typename TheTapeLeft, bf_data_t RDataHead, bf_data_t... RDataTail, typename TheInput, typename TheOutput >
 struct Program< TheCode, '+', TheTapeLeft, TapeRight<RDataHead, RDataTail...>, TheInput, TheOutput >
 {
-	using run = typename Program< TheCode, command_next, TheTapeLeft, TapeRight< RDataHead + 1, RDataTail... >, TheInput, TheOutput >::run;
+  using run = typename Program< TheCode, command_next, TheTapeLeft, TapeRight< RDataHead + 1, RDataTail... >, TheInput, TheOutput >::run;
 };
 
 // command '-'
 template < typename TheCode, typename TheTapeLeft, bf_data_t RDataHead, bf_data_t... RDataTail, typename TheInput, typename TheOutput >
 struct Program< TheCode, '-', TheTapeLeft, TapeRight<RDataHead, RDataTail...>, TheInput, TheOutput >
 {
-	using run = typename Program< TheCode, command_next, TheTapeLeft, TapeRight< RDataHead - 1, RDataTail... >, TheInput, TheOutput >::run;
+  using run = typename Program< TheCode, command_next, TheTapeLeft, TapeRight< RDataHead - 1, RDataTail... >, TheInput, TheOutput >::run;
 };
 
 // command '>'
 template < typename TheCode, bf_data_t... LData, bf_data_t RDataHead, bf_data_t... RDataTail, typename TheInput, typename TheOutput >
 struct Program< TheCode, '>', TapeLeft<LData...>, TapeRight<RDataHead, RDataTail...>, TheInput, TheOutput >
 {
-	using run = typename Program< TheCode, command_next, TapeLeft<RDataHead, LData...>, TapeRight< RDataTail... >, TheInput, TheOutput >::run;
+  using run = typename Program< TheCode, command_next, TapeLeft<RDataHead, LData...>, TapeRight< RDataTail... >, TheInput, TheOutput >::run;
 };
 
 // command '<'
 template < typename TheCode, bf_data_t LDataHead, bf_data_t... LDataTail, bf_data_t... RData, typename TheInput, typename TheOutput >
 struct Program< TheCode, '<', TapeLeft<LDataHead, LDataTail...>, TapeRight<RData...>, TheInput, TheOutput >
 {
-	using run = typename Program< TheCode, command_next, TapeLeft< LDataTail...>, TapeRight< LDataHead, RData... >, TheInput, TheOutput >::run;
+  using run = typename Program< TheCode, command_next, TapeLeft< LDataTail...>, TapeRight< LDataHead, RData... >, TheInput, TheOutput >::run;
 };
 
 template < typename TheCode, bf_data_t... RData, typename TheInput, typename TheOutput >
 struct Program< TheCode, '<', TapeLeft<>, TapeRight<RData...>, TheInput, TheOutput >
 {
-	using run = typename Program< TheCode, command_next, TapeLeft<>, TapeRight< data_zero, RData... >, TheInput, TheOutput >::run;
+  using run = typename Program< TheCode, command_next, TapeLeft<>, TapeRight< data_zero, RData... >, TheInput, TheOutput >::run;
 };
 
 
 // printing helper
 template <class T> struct PrintOutput;
 template < bf_data_t first, bf_data_t... OData > struct PrintOutput< Output< first, OData... > > {
-	void operator()() { putchar(first); PrintOutput< Output< OData... > >()(); }
+  void operator()() { putchar(first); PrintOutput< Output< OData... > >()(); }
 };
 template <> struct PrintOutput< Output<> > { void operator()() { putchar('\n'); } };
 template < typename Output > void print_output(Output o=Output())
 {
-	PrintOutput< Output >()();
+  PrintOutput< Output >()();
 }
 
 
 
 int main()
 {
-	{
-		using the_code = Code<'<', '>', ',', '+', '.', '+', '+', '-', '-', '-', '.', '<', '.'>;
-		using the_input = Input<'X'>;
-		using the_tape_left = TapeLeft<'K'>;
-		using the_tape_right = TapeRight<'1', '2'>;
-		using the_output = Output<'H', 'E', 'L', 'L', 'O'>;
-		print_output(Program< CodeStack< Code<>, the_code >, command_next, the_tape_left, the_tape_right, the_input, the_output >::run());
-	}
+  {
+    using the_code = Code<'<', '>', ',', '+', '.', '+', '+', '-', '-', '-', '.', '<', '.'>;
+    using the_input = Input<'X'>;
+    using the_tape_left = TapeLeft<'K'>;
+    using the_tape_right = TapeRight<'1', '2'>;
+    using the_output = Output<'H', 'E', 'L', 'L', 'O'>;
+    print_output(Program< CodeStack< Code<>, the_code >, command_next, the_tape_left, the_tape_right, the_input, the_output >::run());
+  }
 
-	{
-		using the_code = Code<'+', ']', '-', '+', '+', '.'>;
-		using the_input = Input<'X'>;
-		using the_tape_left = TapeLeft<>;
-		using the_tape_right = TapeRight<'K'>;
-		using the_output = Output<>;
-		print_output(Program< CodeStack< Code<>, the_code >, command_next, the_tape_left, the_tape_right, the_input, the_output >::run());
-	}
-    return 0;
+  {
+    using the_code = Code<'+', '+', '+', '[', '-', '.', ']', '+', '+', '.'>;
+    using the_input = Input<'X'>;
+    using the_tape_left = TapeLeft<>;
+    using the_tape_right = TapeRight<>;
+    using the_output = Output<>;
+    PrintOutput< Program< CodeStack< Code<>, the_code >, command_next, the_tape_left, the_tape_right, the_input, the_output >::run >()();
+  }
+  return 0;
 }
